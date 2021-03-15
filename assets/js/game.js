dc70d6d1abcc116f265ec13dd12114d0ca6b9538
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-03-15 17:16:12 -0500

    Fight Troubleshooting

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,6 +19,8 @@ var fight = function(enemyName) {
     console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
     // if player picks "skip" confirm and then stop the loop
+
+    // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
       // confirm player wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
@@ -95,6 +97,30 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+// function to start a new game
+var startGame = function() {
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+   // play again
+   startGame();
+};
+
+// start the game when the page loads
+startGame();
 
 
 //fight();
